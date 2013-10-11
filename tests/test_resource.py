6a22570dbd5a0963fc5e21cc961a4710cfd91a8e
Lorenzo Masini <rugginoso@develer.com>
2013-10-11 18:47:19 +0200

    Added rule generation tests for resource.

---
+++ tests/test_resource.py
@@ -0,0 +1,74 @@
+import sys
+sys.path.append('..')
+
+import unittest
+
+from dread.base import BaseResource
+
+
+class TestRuleGeneration(unittest.TestCase):
+    def setUp(self):
+        def endpoint(self, params):
+            pass
+
+        self.resource = BaseResource()
+        self.endpoint = endpoint
+
+
+    def test_list(self):
+        self.resource.on_list = self.endpoint
+
+        rules = list(self.resource.get_rules(None))
+
+        assert len(rules) == 1
+        assert rules[0].rule == '/baseresource/'
+        assert rules[0].endpoint == self.endpoint
+        assert rules[0].methods == {'GET', 'HEAD'}
+
+
+    def test_create(self):
+        self.resource.on_create = self.endpoint
+
+        rules = list(self.resource.get_rules(None))
+
+        assert len(rules) == 1
+        assert rules[0].rule == '/baseresource/'
+        assert rules[0].endpoint == self.endpoint
+        assert rules[0].methods == {'POST'}
+
+
+    def test_detail(self):
+        self.resource.on_detail = self.endpoint
+
+        rules = list(self.resource.get_rules(None))
+
+        assert len(rules) == 1
+        assert rules[0].rule == '/baseresource/<int:baseresource_id>'
+        assert rules[0].endpoint == self.endpoint
+        assert rules[0].methods == {'GET', 'HEAD'}
+
+
+    def test_update(self):
+        self.resource.on_update = self.endpoint
+
+        rules = list(self.resource.get_rules(None))
+
+        assert len(rules) == 1
+        assert rules[0].rule == '/baseresource/<int:baseresource_id>'
+        assert rules[0].endpoint == self.endpoint
+        assert rules[0].methods == {'PUT'}
+
+
+    def test_delete(self):
+        self.resource.on_delete = self.endpoint
+
+        rules = list(self.resource.get_rules(None))
+
+        assert len(rules) == 1
+        assert rules[0].rule == '/baseresource/<int:baseresource_id>'
+        assert rules[0].endpoint == self.endpoint
+        assert rules[0].methods == {'DELETE'}
+
+
+if __name__ == '__main__':
+    unittest.main()
